37fc5953a8f3b3ffaf5518daa1485507225c112b
NewCreate <85439835+NewCreate@users.noreply.github.com>
2021-08-25 20:22:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -67,7 +67,7 @@ async def start(bot, cmd):
                 buttons = [
                     [
                         InlineKeyboardButton('Search again', switch_inline_query_current_chat=''),
-                        InlineKeyboardButton('〽️ Share  ➢', url='https://t.me/share/movies_Hubh2')
+                        InlineKeyboardButton('🎬 Group ⚡️', url='https://t.me/movies_Hubh2')
                     ]
                     ]
                 await bot.send_cached_media(
