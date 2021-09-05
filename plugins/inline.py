5adad8267c6e63559bd6f4bfbe6c47b64813f69c
NewCreate <85439835+NewCreate@users.noreply.github.com>
2021-09-05 10:34:32 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again 🔎', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('〽️ Share  ➢', url='https://t.me/share/url?url=https://t.me/movies_Hubh2')
+            InlineKeyboardButton('〽️ Share Now  ➢', url='https://t.me/share/url?url=https://t.me/movies_Hubh2')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
