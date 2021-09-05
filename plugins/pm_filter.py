0dc14b35423ee7133a12895b07de132068bfc597
NewCreate <85439835+NewCreate@users.noreply.github.com>
2021-09-05 22:54:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -74,7 +74,7 @@ async def filter(client, message):
                 file_id = file.file_id
                 filename = f"〽️ [{get_size(file.file_size)}] 🎬 {file.file_name}"
                 btn.append(
-                    [InlineKeyboardButton(text=f"{filename}",callback_data=f"subinps#{file_id}")]
+                    [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=f"subinps#{file_id}")]
                     )
         else:
             await client.send_sticker(chat_id=message.from_user.id, sticker='CAADBQADMwIAAtbcmFelnLaGAZhgBwI')
