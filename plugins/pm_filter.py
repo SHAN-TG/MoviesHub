a1c33d87bab930b172c9ef67ffd0b22da5caaff8
NewCreate <85439835+NewCreate@users.noreply.github.com>
2021-09-05 11:08:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -102,7 +102,7 @@ async def filter(client, message):
         buttons = data['buttons'][0].copy()
 
         buttons.append(
-            [InlineKeyboardButton(text="Go to Next Page   ➪",callback_data=f"next_0_{keyword}")]
+            [InlineKeyboardButton(text="Go To Next Page   ➪",callback_data=f"next_0_{keyword}")]
         )    
         buttons.append(
             [InlineKeyboardButton(text=f"📖  Pages 1/{data['total']}  📚",callback_data="pages")]
@@ -165,7 +165,7 @@ async def group(client, message):
         buttons = data['buttons'][0].copy()
 
         buttons.append(
-            [InlineKeyboardButton(text="Go to Next Page  ➪",callback_data=f"next_0_{keyword}")]
+            [InlineKeyboardButton(text="Go To Next Page  ➪",callback_data=f"next_0_{keyword}")]
         )    
         buttons.append(
             [InlineKeyboardButton(text=f"📖  Pages 1/{data['total']}  📚",callback_data="pages")]
