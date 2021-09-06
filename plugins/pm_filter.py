6bb4f08aaa75229fc9e9ac2ad4db32cf29653d98
NewCreate <85439835+NewCreate@users.noreply.github.com>
2021-09-06 07:21:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -13,7 +13,7 @@ RATING = ["5.1 | IMDB", "6.2 | IMDB", "7.3 | IMDB", "8.4 | IMDB", "9.5 | IMDB", 
 GENRES = ["fun, fact",
          "Thriller, Comedy",
          "Drama, Comedy",
-         "Family, Drama",
+         "Family,🔪Drama",
          "Action, Adventure",
          "Film Noir",
          "Documentary"]
