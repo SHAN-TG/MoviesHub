fecf53beb93bbfa956ef065777e5ddb431f122f4
NewCreate <85439835+NewCreate@users.noreply.github.com>
2021-09-06 07:39:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -273,7 +273,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 buttons = data['buttons'][int(index)-1].copy()
 
                 buttons.append(
-                    [InlineKeyboardButton("《  BACK", callback_data=f"back_{int(index)-1}_{keyword}"),InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{int(index)-1}_{keyword}")]
+                    [InlineKeyboardButton("《  BACK", callback_data=f"back_{int(index)-1}_{keyword}"),InlineKeyboardButton("NEXT  ➪", callback_data=f"next_{int(index)-1}_{keyword}")]
                 )
                 buttons.append(
                     [InlineKeyboardButton(f"📖  Pages {int(index)}/{data['total']}  📚", callback_data="pages")]
