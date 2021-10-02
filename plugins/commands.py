3df9505b8fde72303128078569de47214a871ad1
NewCreate <85439835+NewCreate@users.noreply.github.com>
2021-10-02 08:43:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -66,7 +66,7 @@ async def start(bot, cmd):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('Search again 🔎', switch_inline_query_current_chat=''),
+                        InlineKeyboardButton('🔍 Search again ', switch_inline_query_current_chat=''),
                         InlineKeyboardButton('〽️ Share Now  ➢', url='https://t.me/share/url?url=https://t.me/movies_Hubh2')
                     ]
                     ]
