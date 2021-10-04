dae719d5d9c61f47d44448cc6d76240bc061d838
NewCreate <85439835+NewCreate@users.noreply.github.com>
2021-10-04 11:59:27 +0530

    Create commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -66,7 +66,7 @@ async def start(bot, cmd):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton(' Search again 🔎', switch_inline_query_current_chat=''),
+                        InlineKeyboardButton('🔍 Search again ', switch_inline_query_current_chat=''),
                         InlineKeyboardButton(' Share Now  ➢', url='https://t.me/share/url?url=https://t.me/movies_Hubh2')
                     ]
                     ]
@@ -96,7 +96,7 @@ async def start(bot, cmd):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton(" Search Here 🔎", switch_inline_query_current_chat=''),
+                        InlineKeyboardButton("🔍 Search Here ", switch_inline_query_current_chat=''),
                         InlineKeyboardButton("〽️ New Release ", url="https://t.me/joinchat/aG4_u8iHx31mNzVl")
                     ],
                     [
